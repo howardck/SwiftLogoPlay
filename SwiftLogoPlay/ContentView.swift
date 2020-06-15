82c025e1b42fa08232ccb6fceb0ffd93f281f9f7
howardckatz <howardk@fatdog.com>
2020-06-15 10:05:01 -0700

    init

---
+++ SwiftLogoPlay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftLogoPlay
+//
+//  Created by Howard Katz on 2020-06-15.
+//  Copyright © 2020 Howard Katz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
